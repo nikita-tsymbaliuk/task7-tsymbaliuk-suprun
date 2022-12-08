a4f796a812766a9112b2b3761006b69d5f9558e6
Nikita Tsymbaliuk <ntsymbaliuk@kse.org.ua>
2022-12-08 18:30:08 +0200

    file rename

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import sys
 
+file_with_data = sys.argv[1]
 print(sys.argv[1:])
 
-country = sys.argv[4]
-print(country)
+country = sys.argv[3]
+year = sys.argv[]
+print("country is", country)
 
-with open("Olympic Athletes - athlete_events.tsv", "r") as file:
-    while True:
-        line = file.readline()
-        if not line:
-            break
-        print(line)
+
+
+# with open("Olympic Athletes - athlete_events.tsv", "r") as file:
+#     while True:
+#         line = file.readline()
+#         if not line:
+#             break
+#         print(line)
 
