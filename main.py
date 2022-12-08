700da0943eb60524712cea0476db69367a6a1387
Nikita Tsymbaliuk <ntsymbaliuk@kse.org.ua>
2022-12-08 20:10:07 +0200

    sort by year

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ year = sys.argv[4]
 print("country is", country, "year is", year)
 
 
-
+year_set = set()
 with open(file_with_data, 'r') as file:
+    line = file.readline()
     line = file.readline()
     while line != "":
         line_splitted = line.split("\t")
-        if country == line_splitted[7]:
-            print(line_splitted)
-
+        year = int(line_splitted[9])
+        year_set.add(year)
         line = file.readline()
 
+year_list = sorted(year_set)
+for i,y in enumerate(year_list[:10], 1):
+    print(i, "\t", y)
 
+# set cortage
 #     current_country = [6]
 # print(current_country)
 
