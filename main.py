608478baf2378a4ad0c253dca307966e71498999
Ivan Suprun <suprun.ivanko@gmail.com>
2022-12-08 15:27:28 +0200

    added open("Olympic Athletes - athlete_events.tsv")

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import sys
 
-print(len(sys.argv[1:]))
+print(sys.argv[1:])
 
-country = sys.argv[3]
+country = sys.argv[4]
 print(country)
+
+with open("Olympic Athletes - athlete_events.tsv", "r") as file:
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        print(line)
 
