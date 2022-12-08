f66bbada107ad683b954ed7dd0d04628e792f889
Nikita Tsymbaliuk <ntsymbaliuk@kse.org.ua>
2022-12-08 19:10:22 +0200

    while command beginning

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import sys
 
 file_with_data = sys.argv[1]
-print(sys.argv[1:])
-
 country = sys.argv[3]
 year = sys.argv[4]
-medals = sys.argv[2]
-print("country is", country, "year is", year, "medals are", medals)
+
+# print(sys.argv[1:])
+
+print("country is", country, "year is", year)
 
 
 
-# with open("Olympic Athletes - athlete_events.tsv", "r") as file:
-#     while True:
-#         line = file.readline()
-#         if not line:
-#             break
-#         print(line)
+with open(file_with_data, 'r') as file:
+    line = file.readline()
+    while line != "":
+        line = file.readline()
+        line_splitted = line.split()
+        print(line_splitted)
+
+#     current_country = [6]
+# print(current_country)
 
